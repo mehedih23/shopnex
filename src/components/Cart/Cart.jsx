150acfabe4c553e1f8333427b3264b616f132d31
mehedih23 <nmehedi23@gmail.com>
2023-05-23 22:31:54 +0600

    simple calc done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,18 +4,25 @@ import './Cart.css'
 const Cart = ({selectedProduct}) => {
     console.log(selectedProduct);
     let quantity = 0;
+    let total = 0;
+    let shipping = 0;
     for(const item of selectedProduct){
         quantity = quantity + item.quantity;
+        total = total + item.price * item.quantity;
+        shipping = shipping + item.shipping;
     }
+
+    let tax = total * 0.1;
+    let grandTotal = total + shipping + tax;
 
     return (
         <div className='leading-loose sticky top-0'>
             <h3 className='p-4 text-center font-semibold'>Order Summary</h3>
             <p>Selected Items: {quantity}</p>
-            <p>Total Price: $1140</p>
-            <p>Total Shipping Charge: $5</p>
-            <p>Tax: $114</p>
-            <p>Grand Total: $1559</p>
+            <p>Total Price: $ {total}</p>
+            <p>Total Shipping : $ {shipping}</p>
+            <p>Tax: $ {tax.toFixed(2)}</p>
+            <p>Grand Total: $ {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
